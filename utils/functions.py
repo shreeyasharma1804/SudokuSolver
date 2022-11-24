1bf0feba05652ff411bdb5e4f57d9ccc17f23bdd
Rithika <rithikashetty3010@gmail.com>
2022-11-24 13:02:37 +0530

    functions updated

--- utils/functions.py
+++ utils/functions.py
@@ -6,6 +6,7 @@ def proc(image):
     proc_image=cv2.adaptiveThreshold(blurred_image,255,1,1,11,2)
     return proc_image
 
+
 def biggestContour(contours):
     biggest=np.array([])
     maxArea=0
@@ -18,3 +19,15 @@ def biggestContour(contours):
                 biggest=sides_approx
                 maxArea=area
     return biggest,maxArea
+
+def reorder(mypoints):
+    mypoints=mypoints.reshape((4,2))
+    mypointsNew=np.zeros((4,1,2),dtype=np.int32)
+    sum=mypoints.sum(1)
+    mypointsNew[0]=mypoints[np.argmin(sum)]
+    mypointsNew[3]=mypoints[np.argmax(sum)]
+    sub=np.diff(mypoints,axis=1)
+    mypointsNew[1]=mypoints[np.argmin(sub)]
+    mypointsNew[2]=mypoints[np.argmax(sub)]
+    return mypointsNew
+    
