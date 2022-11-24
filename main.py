3e8e54d1e7fb63fec5d10b0a7c7863a4a869559e
Rithika <rithikashetty3010@gmail.com>
2022-11-24 12:35:48 +0530

    Contouring

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ if __name__ == "__main__":
     import numpy as np
     import os
     import matplotlib.pyplot as plt
-    
-    from utils import proc as proc
+    from utils import proc,biggestContour
 #%% Reading the image file
     path = os.getcwd()
     Im=cv2.imread(os.path.join(path ,'utils','suduko_image.jpg'),0)
     Image=cv2.resize(Im,(256,256))
-    # blank_image=np.zeros((256,256,3),np.unit8)
 #%%Correcting the image and contouring
-    corrected_image=proc(Image)
+    corrected_image=proc.proc(Image)
     imgContour=Image.copy()
     imgBigContour=Image.copy()
     contours, hierarchy=cv2.findContours(corrected_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(imgContour,contours, -1,(0,255,0),3)
-    plt.imshow(imgContour)
+    biggest,maxArea=biggestContour.biggestContour(contours)
+    plt.imshow(biggest)
+    plt.show()
     
