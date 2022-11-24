fb18f7260349dda71e57ece04de45fda40c7cb81
Rithika <rithikashetty3010@gmail.com>
2022-11-24 12:46:08 +0530

    updated

--- utils/proc.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-
-def proc(image):
-    blurred_image=cv2.GaussianBlur(image,(5,5),1)
-    proc_image=cv2.adaptiveThreshold(blurred_image,255,1,1,11,2)
-    return proc_image
