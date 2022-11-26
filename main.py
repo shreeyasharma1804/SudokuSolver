45c8f64b8bac85827cfd163159dc5c20592bd6bb
Rithika <rithikashetty3010@gmail.com>
2022-11-26 16:40:18 +0530

    updated

--- main.py
+++ main.py
@@ -5,19 +5,42 @@ if __name__ == "__main__":
     import os
     import matplotlib.pyplot as plt
     from utils import functions
+    model=functions.initializePredictionModel()
 #%% Reading the image file
     path = os.getcwd()
     Im=cv2.imread(os.path.join(path ,'utils','suduko_image.jpg'),0)
-    Image=cv2.resize(Im,(256,256))
+    widthImg=360
+    heightImg=360
+    constant=255
+    Image=cv2.resize(Im,(widthImg,heightImg))
+    blank_image=np.zeros((heightImg,widthImg,3),np.uint8)
 #%%Correcting the image and contouring
     corrected_image=functions.proc(Image)
     imgContour=Image.copy()
     imgBigContour=Image.copy()
+
     contours, hierarchy=cv2.findContours(corrected_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(imgContour,contours, -1,(0,255,0),3)
+    cv2.drawContours(imgContour,contours, -1,(0,constant,0),3)
     biggest,maxArea=functions.biggestContour(contours)
-    if biggest !=0:
-        biggest =functions.reorder(biggest)
-    plt.imshow(biggest)
-    plt.show()
-    
+    if biggest.size !=0:
+        biggest_reorder =functions.reorder(biggest)
+        cv2.drawContours(imgBigContour,biggest_reorder,-1,(0,constant,0),10)
+        points1=np.float32(biggest_reorder)
+        points2=np.float32([[0,0],[widthImg,0],[0,heightImg],[widthImg,heightImg]])
+        matrix=cv2.getPerspectiveTransform(points1, points2)
+        #%%Splitting the digits
+        imageWarpColor=cv2.warpPerspective(Image,matrix,(widthImg,heightImg))
+        imageDetectedDigits=blank_image.copy()
+        # imageWarpColor=cv2.cvtColor(imageWarpColor,cv2.COLOR_BGR2GRAY)
+        imageSolvedDigits=blank_image.copy()
+        print(imageWarpColor.shape)
+        boxes=functions.splitBoxes(imageWarpColor)
+        # plt.imshow(boxes[2])
+        # plt.show()
+        print(len(boxes))
+        #calling the model 
+        numbers=functions.getPredection(boxes,model)
+        print(numbers)
+        # imageDetectedDigits=functions.displayNumbers(imageDetectedDigits,numbers,color=(255,0,255))
+        # numbers=np.asarray(numbers)
+        # positive_array=np.where(numbers>0,0,1)
